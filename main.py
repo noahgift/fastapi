1dc0013cee9b78648b7f7fcc2d0fdc491d29263b
Noah Gift <noah.gift@gmail.com>
2021-08-31 16:48:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World2"}
 
 @app.get("/add/{num1}/{num2}")
 async def add(num1: int, num2: int):
